c4e86a86fd13bed0039667b527bc577232b9a75b
gearbx <thegearbox@protonmail.com>
2021-11-27 14:22:30 -0700

    Update install script. Add place to /usr/local/bin step.

--- install.sh
+++ install.sh
@@ -1,2 +1,5 @@
-pip install beautifulsoup4
-
+#!/bin/bash
+echo "Install Python dependencies..."
+pip install -r requirements.txt
+echo "Copying script to /usr/local/bin..."
+sudo cp ./webplow.py /usr/local/bin
